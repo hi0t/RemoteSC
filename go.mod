713ed01dfde2e693c2c9d22b969cfa0068ca1305
hi0t <27971344+hi0t@users.noreply.github.com>
2022-12-17 17:15:19 +0200

    wrapping in an interactive service

--- go.mod
+++ go.mod
@@ -3,9 +3,13 @@ module remotesc
 go 1.19
 
 require (
+	github.com/kardianos/service v1.2.2
+	github.com/stretchr/testify v1.8.1
+)
+
+require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/stretchr/objx v0.5.0 // indirect
-	github.com/stretchr/testify v1.8.1 // indirect
+	golang.org/x/sys v0.3.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,49 +1,101 @@
 package main
 
 import (
+	"errors"
+	"flag"
 	"fmt"
 	"log"
 	"os"
-	"os/signal"
+	"remotesc/cmd"
 	"remotesc/server"
+
+	"github.com/kardianos/service"
 )
 
 const DefaultPort = "44555"
 
-func main() {
-	log.SetFlags(0)
+type program struct{}
 
+func (p *program) Start(s service.Service) error {
 	var cfg server.Config
 
 	cfg.Provider = os.Getenv("REMOTESC_PROVIDER")
 	if cfg.Provider == "" {
-		log.Fatal("Provider not set")
+		return errors.New("provider not configured")
 	}
-
 	cfg.Address = os.Getenv("REMOTESC_LISTEN")
 	if cfg.Address == "" {
 		cfg.Address = fmt.Sprintf(":%s", DefaultPort)
 	}
-
 	cfg.Secret = os.Getenv("REMOTESC_SECRET")
 	if cfg.Secret == "" {
-		log.Fatal("Shared secret not set")
+		return errors.New("shared secret not configured")
 	}
-
 	cfg.Cert = os.Getenv("REMOTESC_CERT")
 	if cfg.Cert == "" {
-		log.Fatal("TLS certificate not set")
+		return errors.New("TLS certificate not configured")
 	}
-
 	cfg.Priv = os.Getenv("REMOTESC_PRIV")
 	if cfg.Priv == "" {
-		log.Fatal("Private key not set")
+		return errors.New("private key not configured")
 	}
 
 	server.Start(cfg)
+	return nil
+}
 
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt)
-	<-c
+func (p *program) Stop(s service.Service) error {
 	server.Stop()
+	return nil
+}
+
+func main() {
+	installCmd := flag.NewFlagSet("install", flag.ExitOnError)
+	listen := installCmd.String("listen", fmt.Sprintf(":%s", DefaultPort), "Sets the address and port on which the server will accept requests")
+	provider := installCmd.String("provider", "", "Path to the pkcs11 module")
+
+	log.SetFlags(0)
+	svcConfig := &service.Config{
+		Name:        "RmoteSC",
+		DisplayName: "RmoteSC",
+		Description: "PKCS#11 remote access",
+	}
+
+	prg := &program{}
+	s, err := service.New(prg, svcConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if len(os.Args) > 1 {
+		switch os.Args[1] {
+		case "install":
+			installCmd.Parse(os.Args[2:])
+			if *provider == "" {
+				usage()
+			}
+
+			svcConfig.EnvVars = make(map[string]string)
+			svcConfig.EnvVars["REMOTESC_PROVIDER"] = *provider
+			svcConfig.EnvVars["REMOTESC_LISTEN"] = *listen
+			cmd.Configure(svcConfig.EnvVars)
+
+			if err = s.Install(); err != nil {
+				log.Fatal(err)
+			}
+		default:
+			fmt.Fprintf(os.Stderr, "Unknown subcommand: %s\n", os.Args[1])
+			usage()
+		}
+		os.Exit(0)
+	}
+
+	if err = s.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func usage() {
+	fmt.Fprintf(os.Stderr, "usage: %s install --provider <module> [--listen <address>] \n", os.Args[0])
+	os.Exit(1)
 }
